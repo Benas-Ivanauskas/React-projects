f1279dd5da4aedff204c9f36cdca6bf0a59dfab9
Benas Ivanauskas <benas.ivanauskas@gmail.com>
2024-04-01 20:58:33 +0300

    updating current picked car page responsive ir some js code

--- Book-car-project/src/Pages/CurrentCarPage.jsx
+++ Book-car-project/src/Pages/CurrentCarPage.jsx
@@ -4,51 +4,77 @@ import { FaRegCalendarAlt } from "react-icons/fa";
 import { FaPersonCirclePlus } from "react-icons/fa6";
 import { MdOutlineMiscellaneousServices } from "react-icons/md";
 import { FaLocationDot } from "react-icons/fa6";
+import { useEffect, useState } from "react";
 
 function CurrentCarPage({ cars }) {
   const { id } = useParams();
+  const [text, setText] = useState("");
+  const [activeButton, setActiveButton] = useState("overview");
 
   const currentCar = cars.find((car) => car.id === id);
-  console.log(currentCar);
+
+  useEffect(() => {
+    if (currentCar) {
+      setText(currentCar.overview);
+    }
+  }, [currentCar]);
 
   if (!currentCar) {
     return <div>Car not found</div>;
   }
 
+  const handleClickOverview = () => {
+    let text = currentCar.overview;
+    setText(text);
+    setActiveButton("overview");
+  };
+
+  const handleClickAccessories = () => {
+    let text = currentCar.accessories;
+    setText(text);
+    setActiveButton("accessories");
+  };
+
   return (
     <div className="current-car-page">
       <div className="current-car-container">
         <div className="about-car">
-          <h2>Range Rover Sport</h2>
+          <h2>{currentCar.brand}</h2>
           <div className="specification">
             <p>
-              <FaRegCalendarAlt /> 2022 model year
+              <FaRegCalendarAlt /> {currentCar.model} model year
             </p>
             <p>
-              <FaPersonCirclePlus /> 7 seats
+              <FaPersonCirclePlus /> {currentCar.seats} seats
             </p>
             <p>
-              <MdOutlineMiscellaneousServices /> petrol
+              <MdOutlineMiscellaneousServices /> {currentCar.fuel}
             </p>
           </div>
           <div className="car-description">
             <div>
-              <button className="active">Overview</button>
-              <button>Accessories</button>
+              <button
+                className={activeButton === "overview" ? "active" : ""}
+                onClick={handleClickOverview}
+              >
+                Overview
+              </button>
+              <button
+                className={activeButton === "accessories" ? "active" : ""}
+                onClick={handleClickAccessories}
+              >
+                Accessories
+              </button>
             </div>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam iure
-              repellat quia possimus aspernatur officia reprehenderit maiores
-              dicta provident laboriosam.
-            </p>
+            <p>{text}</p>
           </div>
         </div>
         <div className="form-container">
           <div>
             <p>
-              <FaLocationDot /> Location
+              <FaLocationDot /> {currentCar.location}
             </p>
-            <p>$200 per day</p>
+            <p>${currentCar.cost} per day</p>
           </div>
           <form action="">
             <label htmlFor="pickUp">
--- Book-car-project/src/Pages/components/HomePageCars.jsx
+++ Book-car-project/src/Pages/components/HomePageCars.jsx
@@ -1,20 +1,29 @@
+import { Link } from "react-router-dom";
+
 function HomePageCars({ cars }) {
   const displaySixCars = cars.slice(0, 6);
   return (
     <div className="choose-car">
-      {displaySixCars.map((car, index) => (
-        <div className="car-info" key={index}>
-          <div className="img-container">
-            <img src={car.image} alt={car.brand} />
+      {displaySixCars.map((car) => (
+        <Link
+          to={`/car-listing/car/${car.id}`}
+          style={{ textDecoration: "none", color: "inherit" }}
+          key={car.id}
+          className="car-link"
+        >
+          <div className="car-info">
+            <div className="img-container">
+              <img src={car.image} alt={car.brand} />
+            </div>
+            <h2>{car.brand}</h2>
+            <h4>
+              Location: <span>{car.location}</span>
+            </h4>
+            <h4>
+              Daily price <span>{car.cost}&euro;</span>
+            </h4>
           </div>
-          <h2>{car.brand}</h2>
-          <h4>
-            Location: <span>{car.location}</span>
-          </h4>
-          <h4>
-            Daily price <span>{car.cost}&euro;</span>
-          </h4>
-        </div>
+        </Link>
       ))}
     </div>
   );
