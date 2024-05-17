4123e3831a15780fbe0d772474889ab2cc45d2f8
Benas <146756506+Benas-Ivanauskas@users.noreply.github.com>
2024-05-17 16:47:28 +0300

    Delete shoes-shop-project directory

--- shoes-shop-project/src/components/buttonForProduct.jsx
+++
@@ -1,8 +0,0 @@
-import "./buttonForProduct.css";
-export default function ButtonFromProduct({ children }) {
-  return (
-    <div>
-      <button className="buttonForProduct">{children}</button>
-    </div>
-  );
-}
